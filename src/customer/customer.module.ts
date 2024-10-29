f764eb3330515b7b886d982f42d6feed980132a0
atiqBytes <atiqbytes@gmail.com>
2024-10-29 12:13:51 +0500

    made a 'jwt auth guard' and 'token' service to take the funcionality from 'customer verification' endpoint and put it in guard

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { TokenService } from './services/token.service';
 
 @Module({
   imports:[HttpModule],
   controllers: [CustomerController],
-  providers: [CustomerService],
+  providers: [CustomerService,TokenService],
 })
 export class CustomerModule {}
