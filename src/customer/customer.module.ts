75d06f67aebb917eb9d698c166267fb7e9d1a3a7
atiqbitstream <atiq.bitstream@gmail.com>
2024-10-29 23:17:31 +0500

    created basic 'customer' endpoints like create, get and update and made a database connection

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -3,9 +3,11 @@ import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { TokenService } from './services/token.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './entities/customer.entity';
 
 @Module({
-  imports:[HttpModule],
+  imports:[HttpModule,TypeOrmModule.forFeature([Customer])],
   controllers: [CustomerController],
   providers: [CustomerService,TokenService],
 })
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { Customer } from './entities/customer.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
 
 @Injectable()
 export class CustomerService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+
+
+  constructor( @InjectRepository(Customer)
+  private customersRepository: Repository<Customer>,){}
+
+  create(newCustomer: CreateCustomerDto) {
+
+    const customer = this.customersRepository.create(newCustomer);
+
+    const savedCustomer = this.customersRepository.save(customer);
+
+    return savedCustomer;
   }
 
   findAll() {
@@ -13,14 +27,25 @@ export class CustomerService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} customer`;
+    return this.customersRepository.findOneBy({
+      id
+    })
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(id: number, updateCustomer: UpdateCustomerDto) {
+
+    await this.customersRepository.update(id,updateCustomer);
+
+    return this.customersRepository.findOneBy({
+        id
+      });
+    
   }
 
   remove(id: number) {
     return `This action removes a #${id} customer`;
   }
 }
+
+
+
