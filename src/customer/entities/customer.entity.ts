a5d9caa8d424a2e2805c7a48511d2c023417ca1e
atiqbitstream <atiqbitstream@gmail.com>
2024-10-29 21:24:21 +0500

    filled the 'customer' entity

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,41 @@
-export class Customer {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Customer {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    phoneNumber:number;
+
+    @Column()
+    Address:string;
+
+    @Column()
+    sector:string;
+
+    @Column()
+    street:string;
+
+    @Column()
+    googlePin:string;
+
+    @Column()
+    homePicture:string;
+
+    @Column()
+    organization:string;
+
+    @Column()
+    status:boolean;
+
+    @Column()
+    contract:string;
+}
