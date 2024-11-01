232cf69c831576c3024651ddba69d964ad1690ca
atiqBytes <atiqbytes@gmail.com>
2024-11-01 22:24:58 +0500

    minors

--- src/rider/dto/update-rider.dto.ts
+++ src/rider/dto/update-rider.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRiderDto } from './create-rider.dto';
 
-export class UpdateRiderDto extends PartialType(CreateRiderDto) {}
+export class UpdateRiderDto extends PartialType(CreateRiderDto) {
+
+    firstName?: string;
+    lastName?: string;
+    phoneNumber?: string;
+    address?: string;
+    sector?: string;
+    street?: string;
+    cnicNumber?:string;
+    updatedAt:Date;
+    createdAt:Date;
+}
