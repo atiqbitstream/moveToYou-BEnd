fbfd95ac6e1a3515bd58c3d25659407ba2edceb5
atiqbitstream <atiqbitstream@gmail.com>
2024-11-01 21:45:55 +0500

    made the relationship between 'product' and 'deliveryitem' entities

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,16 @@
-export class Product {}
+import { DeliveryItem } from 'src/rider/entities/deliveryItem.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(()=>DeliveryItem,(deliveryItem)=>deliveryItem.product)
+   deliveryItems:DeliveryItem[];
+
+}
