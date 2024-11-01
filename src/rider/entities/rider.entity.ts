beec73791a3f0dc19dd377dc61392d3e2dab9943
atiqBytes <atiqbytes@gmail.com>
2024-11-01 15:23:17 +0500

    starting from 'rider' module , created a 'rider' entity

--- src/rider/entities/rider.entity.ts
+++ src/rider/entities/rider.entity.ts
@@ -1,1 +1,29 @@
-export class Rider {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Rider {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    phoneNumber:string;
+
+    @Column()
+    address:string;
+
+    @Column()
+    sector:string;
+
+    @Column()
+    street:string;
+
+    @Column()
+    cnicNumber:string;
+
+}
