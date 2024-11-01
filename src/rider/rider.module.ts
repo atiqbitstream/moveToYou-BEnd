52873d79bf9dbca8be7a5ced35aab6d1d7c415e4
atiqBytes <atiqbytes@gmail.com>
2024-11-02 00:14:50 +0500

    "CRU" for rider entity

--- src/rider/rider.module.ts
+++ src/rider/rider.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RiderService } from './rider.service';
 import { RiderController } from './rider.controller';
+import { Rider } from './entities/rider.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [RiderController],
+  imports:[TypeOrmModule.forFeature([Rider])],
+  controllers: [RiderController, ],
   providers: [RiderService],
 })
 export class RiderModule {}
--- src/rider/rider.service.ts
+++ src/rider/rider.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRiderDto } from './dto/create-rider.dto';
 import { UpdateRiderDto } from './dto/update-rider.dto';
+import { Rider } from './entities/rider.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RiderService {
-  create(createRiderDto: CreateRiderDto) {
-    return 'This action adds a new rider';
+
+  constructor( @InjectRepository(Rider)
+  private ridersRepository: Repository<Rider>,){}
+
+
+  create(newRider: CreateRiderDto) {
+
+    const rider = this.ridersRepository.create(newRider);
+
+    const savedRider = this.ridersRepository.save(rider);
+
+    return savedRider;
   }
 
   findAll() {
@@ -13,11 +26,18 @@ export class RiderService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} rider`;
+    return this.ridersRepository.findOneBy({
+      id
+    })
   }
 
-  update(id: number, updateRiderDto: UpdateRiderDto) {
-    return `This action updates a #${id} rider`;
+  async update(id: number, updateRider: UpdateRiderDto) {
+
+    await this.ridersRepository.update(id,updateRider);
+
+    return this.ridersRepository.findOneBy({
+      id
+    })
   }
 
   remove(id: number) {
