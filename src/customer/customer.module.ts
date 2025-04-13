71a4e87ed92d9644b3f8b4fd9b714feb62cdc9e3
atiqbitstream <atiq.bitstream@gmail.com>
2025-04-13 11:20:02 +0500

    Configure CustomerModule with related entities, services, and forwardRef to resolve circular dependency with RiderModule

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,4 +1,7 @@
-import { Module } from '@nestjs/common';
+import { RiderModule } from './../rider/rider.module';
+import { DeliveryItem } from './../rider/entities/deliveryItem.entity';
+import { DailyDelivery } from 'src/rider/entities/dailyDelivery.entity';
+import { forwardRef, Module } from '@nestjs/common';
 import { CustomerService } from './services/customer.service';
 import { CustomerController } from './customer.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
@@ -6,11 +9,19 @@ import { TokenService } from '../shared/services/token.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
 import { Route } from 'src/rider/entities/route.entity';
+import { Invoice } from './entities/invoice.entity';
+import { Receipt } from './entities/receipt.entity';
+import { ReceiptService } from './services/receipt.service';
 
 @Module({
-  imports:[HttpModule,TypeOrmModule.forFeature([Customer,Route])],
+  imports: [
+    HttpModule,
+    forwardRef(() => RiderModule),  // Fix circular dependency
+    TypeOrmModule.forFeature([Customer, Route, DailyDelivery, DeliveryItem, Invoice, Receipt])
+  ],
   controllers: [CustomerController],
-  providers: [CustomerService,TokenService],
-  exports:[CustomerService]
+  providers: [CustomerService, TokenService, ReceiptService],
+  exports: [CustomerService, ReceiptService]
+
 })
 export class CustomerModule {}
